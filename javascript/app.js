b9a58db165029fedce6d72ea255cd4d8f0cba208
abdallakhattab <khattababdalla@yahoo.com>
2021-04-18 20:54:09 +0300

    added some events

--- javascript/app.js
+++ javascript/app.js
@@ -3,10 +3,12 @@
 let right = document.getElementById('right');
 let middle = document.getElementById('middle');
 let left = document.getElementById('left');
-let productsNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg','chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', ' water-can.jpg' , 'wine-glass.jpg'];
+let productsNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', ' water-can.jpg', 'wine-glass.jpg'];
 let productpics = [];
+let attempts = 0;
+let maxattempts = 25;
 
-function Products (productName , path){
+function Products(productName, path) {
   this.productName = productName;
   this.path = path;
   this.vote = 0;
@@ -14,9 +16,9 @@ function Products (productName , path){
   productpics.push(this);
 }
 
-function newProduct (){
-  for(let i = 0 ; i<productsNames.length ; i++ ) {
-    new Products( productsNames[i] ,`../img/${productsNames[i]}` );
+function newProduct() {
+  for (let i = 0; i < productsNames.length; i++) {
+    new Products(productsNames[i], `../img/${productsNames[i]}`);
   }
   console.log(productpics);
 }
@@ -30,9 +32,9 @@ math();
 
 let rightindex = math();
 let middleindex = math();
-let leftindex  = math();
+let leftindex = math();
 
-while(rightindex === middleindex || rightindex === leftindex || middleindex === leftindex){
+while (rightindex === middleindex || rightindex === leftindex || middleindex === leftindex) {
 
   middleindex = math();
   rightindex = math();
@@ -47,8 +49,36 @@ right.src = productpics[rightindex].path;
 // middle.setAttribute('src' , 'productpics[middleindex].path');
 
 console.log(typeof productpics[rightindex].path);
+left.addEventListener('click', click);
+middle.addEventListener('click', click);
+right.addEventListener('click', click);
+function click(event) {
+  attempts++;
+  if (attempts <= maxattempts) {
+    if (event.target.id === right) {
+      Products.productpics[rightindex].vote++;
+      console.log(attempts);
+    }
+    else if (
+      event.target.id === middle) {
+      Products.productpics[middleindex].vote++;
+    }
+    else if (event.target.id === left) {
+      Products.productpics[leftindex].vote++;
+    } else {
+      //renderList();
+    }
+  }
+}
 
 
 
+// function renderList() {
+//   let ul = document.getElementById('unList');
+//   for (let i = 0; i < productsNames.length; i++) {
+//     let li = document.createElement('li');
+//     ul.appendChild(li);
+//     li.textContent = `${productsNames[i].productName} it has ${productsNames[i].votes} Votes`;
+//   }
 
-
+// }
