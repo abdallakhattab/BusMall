1651be3d9057571bc203507e986e44e77986c68d
abdallakhattab <khattababdalla@yahoo.com>
2021-04-18 18:54:29 +0300

    added the random pics

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-let right = document.getElementById('right-img');
-let middle = document.getElementById('middle-img');
-let left = document.getElementById('left-img');
+let right = document.getElementById('right');
+let middle = document.getElementById('middle');
+let left = document.getElementById('left');
 let productsNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg','chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', ' water-can.jpg' , 'wine-glass.jpg'];
 let productpics = [];
 
@@ -21,6 +21,33 @@ function newProduct (){
   console.log(productpics);
 }
 newProduct();
+
+function math() {
+  return Math.floor(Math.random() * productpics.length);
+
+}
+math();
+
+let rightindex = math();
+let middleindex = math();
+let leftindex  = math();
+
+while(rightindex === middleindex || rightindex === leftindex || middleindex === leftindex){
+
+  middleindex = math();
+  rightindex = math();
+}
+
+//left.setAttribute('src' , 'productpics[rightindex].path');
+middle.src = productpics[middleindex].path;
+left.src = productpics[leftindex].path;
+right.src = productpics[rightindex].path;
+// right.setAttribute('src' , 'productpics[rightindex].path');
+// left.setAttribute('src' , 'productpics[leftindex].path');
+// middle.setAttribute('src' , 'productpics[middleindex].path');
+
+console.log(typeof productpics[rightindex].path);
+
 
 
 
