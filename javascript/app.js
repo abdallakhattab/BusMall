fa86f4f15819335f77a25000f245ef6bf79e228c
abdallakhattab <khattababdalla@yahoo.com>
2021-04-19 14:04:49 +0300

    added 123

--- javascript/app.js
+++ javascript/app.js
@@ -8,6 +8,7 @@ let productpics = [];
 let attempts = 0;
 let maxattempts = 25;
 
+
 function Products(productName, path) {
   this.productName = productName;
   this.path = path;
@@ -29,70 +30,78 @@ function math() {
 
 }
 math();
-
-let rightindex = math();
-let middleindex = math();
-let leftindex = math();
-
-while (rightindex === middleindex || rightindex === leftindex || middleindex === leftindex) {
-
-  middleindex = math();
+let rightindex;
+let middleindex;
+let leftindex;
+function renderthreeimages() {
   rightindex = math();
-}
-
-//left.setAttribute('src' , 'productpics[rightindex].path');
-middle.src = productpics[middleindex].path;
-left.src = productpics[leftindex].path;
-right.src = productpics[rightindex].path;
-// right.setAttribute('src' , 'productpics[rightindex].path');
-// left.setAttribute('src' , 'productpics[leftindex].path');
-// middle.setAttribute('src' , 'productpics[middleindex].path');
-
-console.log(typeof productpics[rightindex].path);
-left.addEventListener('click', click);
-middle.addEventListener('click', click);
-right.addEventListener('click', click);
-function click(event) {
-  attempts++;
-  if (attempts <= maxattempts) {
-    if (event.target.right === right) {
-      Products.productpics[rightindex].vote++;
-    }
-    else if (
-      event.target.id === middle) {
-      Products.productpics[middleindex].vote++;
-    }
-    else if (event.target.id === left) {
-      Products.productpics[leftindex].vote++;
-    }
-    else {
-      renderList();
-    }
-    index();
-  }
-}
-click();
-
-
-
-function renderList() {
-  let ul = document.getElementById('unlist');
-  for (let i = 0; i < productsNames.length; i++) {
-    let li = document.createElement('li');
-    ul.appendChild(li);
-    li.textContent = `${productpics[i].productName} it has ${productpics[i].votes} Votes`;
-  }
-
-}
-function index() {
-  let rightindex = math();
-  let middleindex = math();
-  let leftindex = math();
+  middleindex = math();
+  leftindex = math();
 
   while (rightindex === middleindex || rightindex === leftindex || middleindex === leftindex) {
 
     middleindex = math();
     rightindex = math();
   }
+  middle.src = productpics[middleindex].path;
+  left.src = productpics[leftindex].path;
+  right.src = productpics[rightindex].path;
 }
-index();
+renderthreeimages();
+
+//left.setAttribute('src' , 'productpics[rightindex].path');
+
+// right.setAttribute('src' , 'productpics[rightindex].path');
+// left.setAttribute('src' , 'productpics[leftindex].path');
+// middle.setAttribute('src' , 'productpics[middleindex].path');
+
+//console.log(typeof productpics[rightindex].path);
+let cont = document.getElementById('cont');
+cont.addEventListener('click', click);
+
+let btn = document.getElementById('btn');
+btn.addEventListener('click', finish);
+function finish() {
+  renderList();
+
+  cont.removeEventListener('click', click);
+}
+
+function click(event) {
+  attempts++;
+  if (attempts < maxattempts) {
+    productpics[rightindex].show++;
+    productpics[middleindex].show++;
+    productpics[leftindex].show++;
+    if (event.target.id === 'right') {
+      productpics[rightindex].vote++;
+      renderthreeimages();
+      console.log(productpics[rightindex].vote);
+    }
+    else if (
+      event.target.id === 'middle') {
+      productpics[middleindex].vote++;
+      renderthreeimages();
+    }
+    else if (event.target.id === 'left') {
+      productpics[leftindex].vote++;
+      renderthreeimages();
+    }
+
+  }
+  else {
+    finish;
+  }
+}
+
+
+function renderList() {
+  let ul = document.getElementById('unlist');
+  for (let i = 0; i < productsNames.length; i++) {
+
+    let li = document.createElement('li');
+    ul.appendChild(li);
+    li.textContent = `${productpics[i].productName} it has ${productpics[i].vote} Votes and is has ${productpics[i].show} seens`;
+  }
+
+}
